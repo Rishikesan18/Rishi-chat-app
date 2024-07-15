794910a98fb5c9d1ab7ed097f9bbab4d4e30070b
Rishikesan18 <p.rishikesan18@gmail.com>
2024-07-15 19:47:34 +0530

    node

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors({
+    origin: ['http://localhost:3000', 'http://localhost:4200', 'http://localhost:5000']
+}))
