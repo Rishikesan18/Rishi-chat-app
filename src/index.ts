4b75d974539895810355f1c3eb450ab872c5fbb7
Rishikesan18 <p.rishikesan18@gmail.com>
2024-07-16 22:37:27 +0530

    change

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+require('dotenv').config();
+import 'reflect-metadata';
 import express from 'express';
 import cors from 'cors';
 import {routes} from "./routes";
@@ -6,30 +8,32 @@ import { DataSource } from 'typeorm';
 
 const dataSource = new DataSource({
     type:"mysql",
-    host:"localhost",
+    host: process.env.HOST,
     port: 3306,
-    username: "root",
-    password: "password",
-    database: "node_websockets",
-    entities: [],
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    entities: [
+        "src/entities/*.ts"
+    ],
     logging: false,
     synchronize: true
 });
 
 dataSource.initialize().then(connection => {
-    const app = express()
+    const app = express();
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors({
     origin: ['http://localhost:3000', 'http://localhost:4200', 'http://localhost:5000']
-}))
+}));
 
 routes(app);
 
 
 app.listen(8000, () => {
-    console.log('listening to port 8000')
-})
+    console.log('listening to port 8000');
+});
 
-})
+}).catch(error => console.log('Error during Data Source initialization:', error));
 
